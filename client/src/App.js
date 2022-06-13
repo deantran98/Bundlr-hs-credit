7591cbcb2d9ef324933f5f4e678b8cb582bf4d41
Duy Tran <60637571+deantran98@users.noreply.github.com>
2022-06-13 16:07:26 -0400

    added some styling for buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,80 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 import * as api from './api'
 import image from './images/dragon.png'
+import Flip from "react-reveal/Flip";
+import LightSpeed from "react-reveal/LightSpeed";
 
 function App() {
 
-    const [file, setFile] = useState();
-    const [link, setLink] = useState();
+  const [file, setFile] = useState();
+  const [link, setLink] = useState();
 
-    async function upload(e) {
-      e.preventDefault();
+  async function upload(e) {
+    e.preventDefault();
 
-      const startingTime = new Date()
+    const startingTime = new Date()
 
-      const formData = new FormData();
-      formData.append('uploadedFile', file)
-      const {data} = await api.post(formData);
+    const formData = new FormData();
+    formData.append('uploadedFile', file)
+    const { data } = await api.post(formData);
 
-      console.log(data.message);
+    console.log(data.message);
 
-      if (data?.id) {
-        setLink(`https://arweave.net/${data.id}`)
-      }
-
-      const endingTime = new Date()
-      
-      console.log(`Total time for upload is ${Math.abs(endingTime - startingTime) / 1000} seconds`);
-    }
-    
-    const onFileChange = (e) => {
-      setFile(e.target.files[0]);
+    if (data?.id) {
+      setLink(`https://arweave.net/${data.id}`)
     }
 
-    return (
-      <div className="App">
+    const endingTime = new Date()
 
-          <form className = "upload-form" onSubmit = {upload} encType = "multipart/form-data" >
-            <input className = "choose-file" type = "file" onChange = {onFileChange} />
-            <button type = "submit" className = "submit"> Submit </button>
+    console.log(`Total time for upload is ${Math.abs(endingTime - startingTime) / 1000} seconds`);
+  }
+
+  const onFileChange = (e) => {
+    setFile(e.target.files[0]);
+
+    // Get the selected file
+    const displayfile = e.target.files[0];
+    // Get the file name and size
+    const { name: fileName, size } = displayfile;
+    // Convert size in bytes to kilo bytes
+    const fileSize = (size / 1000).toFixed(2);
+    // Set the text content
+    const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+    document.querySelector('.file-name').textContent = fileNameAndSize;
+  }
+
+  return (
+
+    <div className="App">
+      <section className='main-container'>
+        <LightSpeed>
+          <form className="upload-form" onSubmit={upload} encType="multipart/form-data" >
+            <div class="file-input">
+              <input type="file" id="file" class="choose-file" onChange={onFileChange} />
+              <label for="file">
+                Select file
+                <p class="file-name"></p>
+              </label>
+            </div>
+
+            <Flip top><button type="submit" className="submit"><span>Submit</span></button></Flip>
           </form>
+        </LightSpeed>
 
-        {link && 
-            <a href = {link} target = "_blank" rel="noreferrer">
-              <img className = "image" src = {image} alt = "dragon-img" 
-              title = "Obtained for completing Calculus with the hard rubric. Click to view your work">
+        {link &&
+          <a href={link} target="_blank" rel="noreferrer">
+            <img className="image" src={image} alt="dragon-img"
+              title="Obtained for completing Calculus with the hard rubric. Click to view your work">
 
-              </img>
-            </a>
+            </img>
+          </a>
         }
-      </div>
-    );
-  
-  
+      </section>
+    </div>
+  );
+
+
 }
 
 export default App;
